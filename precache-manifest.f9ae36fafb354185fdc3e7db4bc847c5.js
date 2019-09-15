73fe140d8100448e32c017d47c1604a623e32d13
Fahid M <fahidroid@gmail.com>
2019-09-15 11:55:30 +0530

    Updates

---
+++ precache-manifest.f9ae36fafb354185fdc3e7db4bc847c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c19939023c460f02de4cfaa1eb5cc20",
+    "url": "/fampay/index.html"
+  },
+  {
+    "revision": "d72bbcd801fb745ebd8b",
+    "url": "/fampay/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "806a623ec3c2d3f909a1",
+    "url": "/fampay/static/css/main.3d355dbd.chunk.css"
+  },
+  {
+    "revision": "d72bbcd801fb745ebd8b",
+    "url": "/fampay/static/js/2.284fed7c.chunk.js"
+  },
+  {
+    "revision": "806a623ec3c2d3f909a1",
+    "url": "/fampay/static/js/main.e48a506a.chunk.js"
+  },
+  {
+    "revision": "781dadad733d021e6f73",
+    "url": "/fampay/static/js/runtime~main.13ba0d90.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fampay/static/media/logo.5d5d9eef.svg"
+  }
+]);
